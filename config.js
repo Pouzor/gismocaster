3711bdf880fb5a35a80941ce6d831e44355f3609
Remy <remy.jardinet@gmail.com>
2021-01-07 01:31:40 +0100

    Create config.js

---
+++ config.js
@@ -0,0 +1,17 @@
+{
+  "name": "gismocaster",
+  "version": "1.1",
+  "slug": "gismocaster",
+  "description": "gismocaster",
+  "arch": ["amd64", "armv7", "aarch64", "amd64", "i386"],
+  "url": "https://blabla.com/blas",
+  "startup": "application",
+  "boot": "auto",
+  "map": ["config:rw", "ssl"],
+  "options": {},
+  "ports": {
+    "8000/tcp": 8000
+  },
+  "schema": {},
+  "image": "Pouzor/gismocaster"
+}
